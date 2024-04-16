56acfa76d17dbf22d90e843a09b6b3639d65b9aa
Alsu Ismayilzade <131715293+alsu-ismayilzada@users.noreply.github.com>
2024-04-16 23:27:01 +0400

    Update

--- script.js
+++ script.js
@@ -25,7 +25,7 @@ sort.addEventListener('mouseleave', () => {
         arr.sort();
       })
       console.log(logo.src);
-  if(logo.src =="http://127.0.0.1:5500/image/icon_black_decrease.svg"){
+  if(logo.src.includes("icon_black_decrease.svg")){
     logo.src="./image/icon_black_increase.svg";
       let i = 0;
       li.forEach((item)=>{
